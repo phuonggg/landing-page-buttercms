d3d7846e6870da4cd2b764ae11b8228331df42e0
Phuong Tran <tieuphuong.ftu@gmail.com>
2021-07-31 18:25:46 +0900

    add hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -4,9 +4,19 @@ import styles from "./index.module.scss";
 
 const HeroSection = () => {
   return (
-    <>
-      <div>HeroSection</div>
-    </>
+    <div className={styles.heroSection}>
+      <div className={styles.container}>
+        <div className={styles.heroImage}></div>
+        <div className={styles.heroContent}>
+          <div className={styles.heroText}>
+            Line 44:6: React Hook useEffect has a missing dependency:
+            'fetchData'. Either include it or remove the dependency array
+            react-hooks/exhaustive-deps
+          </div>
+          <div className={styles.heroButton}></div>
+        </div>
+      </div>
+    </div>
   );
 };
 export default HeroSection;
