994dc0140bfdceb6f00f127c53864e5e881d6ecc
Phuong Tran <tieuphuong.ftu@gmail.com>
2021-08-01 18:44:23 +0900

    add animation

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,19 +1,32 @@
 import React from "react";
-
+import Blue from "../../assets/img/hero/blue.png";
+import Purple from "../../assets/img/hero/purple.png";
 import styles from "./index.module.scss";
 
-const HeroSection = () => {
+const HeroSection = ({ heading }) => {
   return (
-    <div className={styles.heroSection}>
+    <div onMouseover className={styles.heroSection}>
       <div className={styles.container}>
-        <div className={styles.heroImage}></div>
+        <div className={styles.imgFrame}>
+          <img
+            className={styles.blueImg}
+            // onMouseOver={(e) => (e.currentTarget.src = Purple)}
+            // onMouseOut={(e) => (e.currentTarget.src = Blue)}
+            src={Blue}
+            alt=""
+          />
+          <img
+            className={styles.purpleImg}
+            // onMouseOver={(e) => (e.currentTarget.src = Purple)}
+            // onMouseOut={(e) => (e.currentTarget.src = Blue)}
+            src={Purple}
+            alt=""
+          />
+        </div>
         <div className={styles.heroContent}>
-          <div className={styles.heroText}>
-            Line 44:6: React Hook useEffect has a missing dependency:
-            'fetchData'. Either include it or remove the dependency array
-            react-hooks/exhaustive-deps
-          </div>
-          <div className={styles.heroButton}></div>
+          <div className={styles.heroText}>{heading}</div>
+          <div className={styles.heroButton}>SECURE MY DATA</div>
+          <div className={styles.heroButton}>TAKE A CHANCE</div>
         </div>
       </div>
     </div>
